fb78d94f687a1c566cc25bd7fc897725e920e575
Robert Woolley <rob@robertwoolley.co.uk>
2019-10-04 17:47:49 +0000

    Refactored code

--- directory.rb
+++ directory.rb
@@ -1,3 +1,6 @@
+@students = []
+
+
 def interactive_menu
   students = []
   puts "hello"
@@ -11,11 +14,11 @@ def interactive_menu
 # 3. do what the user has asked
     case selection
     when "1"
-    students = input_students
+    input_students
     when "2"
     print_header
-    print(students)
-    print_footer(students)
+    print
+    print_footer
     when "9"
     exit # this will cause the program to terminate
     else
@@ -35,8 +38,6 @@ def input_students
   puts "Enter the default month for the cohort"
   default_cohort = gets.slice(0..-2)
   puts "Default cohort is #{default_cohort}"
-  # create an empty array
-  students = []
   # get the first name
   puts "Please enter name of student."
   name = gets.slice(0..-2)
@@ -47,11 +48,11 @@ def input_students
       if cohort.empty?
       cohort = default_cohort
     end
-    students << {name: name, cohort: cohort}
-    if students.count == 1 
+    @students << {name: name, cohort: cohort}
+    if @students.count == 1 
       puts "Now we have 1 student."
     else
-      puts "Now we have #{students.count} students."
+      puts "Now we have #{@students.count} students."
     end
     # get another name from the user
     puts "Please enter name of student."
@@ -61,7 +62,6 @@ def input_students
     end
     cohort = gets.slice(0..-2)
   end
-  students
 end
 
 def print_header
@@ -69,11 +69,11 @@ puts "The students by cohort at Makers Academy"
 puts "------------"
 end
 
-def print(students)
+def print
 
     cohort_list = []
     
-    students.each do | x |
+    @students.each do | x |
     cohortx = "#{x[:cohort]}"
     cohort_list.push(cohortx)
     end
@@ -84,7 +84,7 @@ def print(students)
 
       puts "Students in #{x} cohort"
 
-      students.each_with_index do | y, index |
+      @students.each_with_index do | y, index |
         check = "#{y[:cohort]}"
         if check == x 
           indexplusone = index + 1
@@ -95,8 +95,8 @@ def print(students)
 end
 
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 # nothing happens until we call the methods
